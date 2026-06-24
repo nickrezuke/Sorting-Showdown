0f5d59fcc01445b174def7031cd14ce8c9dd8fb5
Nicholas Rezuke <rezuke.n@gmail.com>
2026-06-24 01:29:08 -0400

    Separated calls

--- SortingShowdown.java
+++ SortingShowdown.java
@@ -16,30 +16,121 @@ public class SortingShowdown {
             new IntroSorter(),
             new BucketSorter()
     };
+    
+    public static void runRandomSorts(int listSize, int trialCount) {
+        int[] newArray = new int[listSize];
+        for (int i = 0; i < listSize; i++) {
+            newArray[i] = i + 1; // To get 1,2,3,4,5,.....,1998,1999,2000 for N=2000
+        }
+        newArray = DataGenerator.FisherYatesShuffle(newArray);
 
-    // This prints out the average results of sorting lists of size "listSize" 
-    // over the course of trialCount sorts.  Average results printed. 
-    public static void runRandomTests(int listSize, int trialCount) {
-        // TODO: Implement this
+        runSortsOnArray(newArray, trialCount, "");
     }
 
-    // This prints the sort results using a passed in file(s) to sort
-    public static void runTestsOnFiles(String[] fileNames, int trialCount) {
+    public static void runSortsOnFiles(String[] fileNames, int trialCount) {
         for(int i = 0; i < fileNames.length; i++) {
-            runTestsOnFile(fileNames[i], trialCount);
+            runSortsOnFile(fileNames[i], trialCount);
         }
     }
     public static void runTestsOnFiles(String[]fileNames) {
-        runTestsOnFiles(fileNames, 1);
+        runSortsOnFiles(fileNames, 1);
     }
-    public static void runTestsOnFile(String fileName, int trialCount) {
-        // TODO: Implement this
+    public static void runSortsOnFile(String fileName, int trialCount) {
+        int[] theArray = DataGenerator.getArrayFromFile(fileName);
 
+        runSortsOnArray(theArray, trialCount, fileName);
     }
-    public static void runTestsOnFile(String fileName) {
-        runTestsOnFile(fileName, 1);
+    public static void runSortsOnFile(String fileName) {
+        runSortsOnFile(fileName, 1);
     }
 
+    public static void runSortsOnArray(int[] theArray, int trialCount, String fileName) {
+        // Determine the length of the longest sorting algorithm name
+        // for the calculation of the table formatting dimensions
+        int maxNameLength = 0;
+        for (Sorter alg : algorithms) {
+            if (alg.getName().length() > maxNameLength) {
+                maxNameLength = alg.getName().length();
+            }
+        }
+
+        if(trialCount > 1) {
+            System.out.print("\nAveraging over " + trialCount + " trials of ");
+        } else {
+            System.out.print("\nRunning one sorting trial of ");
+        }
+        if(fileName.length() > 0) {
+            System.out.println("the file: " + fileName);
+        } else {
+            if(theArray.length > 6) {
+                System.out.println("randomly shuffled lists of values ranging [1, 2, 3, ..., " 
+                + (theArray.length-2) + ", " +(theArray.length-1) + ", " + theArray.length + "]");
+            } else {
+                System.out.println("randomly shuffled lists of values [1-" + theArray.length + "]");
+            }
+        }
+        
+        String formatString = "| %-" + Math.max(maxNameLength, 22) + "s | %13s | %11s | %13s |";
+        System.out.println("-".repeat(2 + Math.max(maxNameLength, 22) + 3 + 13 + 3 + 11 + 3 + 13 + 2)); 
+        // Represents the Horizontal bar for this table
+        System.out.format(formatString + "\n", "Sorting Algorithm Name", "# Comparisons", "# Exchanges",
+                "Duration (ns)");
+        System.out.println("=".repeat(2 + Math.max(maxNameLength, 22) + 3 + 13 + 3 + 11 + 3 + 13 + 2));
+        for (Sorter algorithm : algorithms) {
+            // We'll keep track of the cumulative averages
+            double avgComparisons = 0.0;
+            double avgExchanges = 0.0;
+            double avgTime = 0.0;
+            for (int trialNum = 0; trialNum < trialCount; trialNum++) {
+                // For each trial, lets generate a new random list
+                int[] newArray = new int[theArray.length];
+                for (int i = 0; i < theArray.length; i++) {
+                    newArray[i] = i + 1; // To get 1,2,3,4,5,.....,1998,1999,2000 for N=2000
+                }
+                newArray = DataGenerator.FisherYatesShuffle(newArray);
+
+                // Lets sort the array based on the algorithm, and time it in nanoseconds
+                long totalTime = System.nanoTime();
+                SortResult result = algorithm.sort(newArray);
+                totalTime = System.nanoTime() - totalTime;
+
+                // Double Check the array is sorted correctly
+                int[] sortedArr = result.sortedArray();
+                for (int i = 0; i < sortedArr.length - 1; i++) {
+                    // If the current element is greater than the next element, it is not sorted
+                    if (sortedArr[i] > sortedArr[i + 1]) {
+                        throw new IllegalStateException("\n" + algorithm.getName() + " failed to sort the list!");
+                    }
+                }
+
+                // Update averages using cumulative average
+                avgComparisons += (result.numberOfComparisons() - avgComparisons) / (trialNum
+                        + 1.0);
+                avgExchanges += (result.numberOfExchanges() - avgExchanges) / (trialNum +
+                        1.0);
+                avgTime += (totalTime - avgTime) / (trialNum + 1.0);
+
+                // Only print out when the percent meaningfully changes so we're
+                // not printing the same line a million times if its slow
+                if (trialCount < 1000 || (trialNum % Math.max(1, trialCount / 1000) == 0)) {
+                    // Equivelant to \r
+                    System.out.print("\u001b[2K\u001b[G");
+
+                    String progress = String.format("[%d/%d] (%d%%)", trialNum + 1,
+                            trialCount,
+                            (int) ((trialNum + 1) * 100.0 / trialCount));
+                    System.out.format(formatString + " %s", algorithm.getName(), (int) avgComparisons,
+                            (int) avgExchanges, (int) avgTime, progress);
+                    System.out.flush();
+                }
+            }
+            // Equivelant to \r
+            System.out.print("\u001b[2K\u001b[G");
+            System.out.format(formatString + "\n", algorithm.getName(), (int) avgComparisons, (int) avgExchanges,
+                    (int) avgTime);
+        }
+        System.out.println("-".repeat(2 + Math.max(maxNameLength, 22) + 3 + 13 + 3 + 11 + 3 + 13 + 2));
+    }
 
     public static void main(String[] args) {
         // Get the data from the files, replace these strings with your own test
